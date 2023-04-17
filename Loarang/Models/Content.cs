0be7eedf60dd2e61edae0b19bd4e9bf9cba0f79c
shinhyunwoo <show2633@naver.com>
2023-04-18 00:41:58 +0900

    [design]: 컨텐츠 세팅 창 작업, 컨텐츠 세팅 창을 위한 컨텐츠 클래스 추가

---
+++ Loarang/Models/Content.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loarang.Models
+{
+	public class Content
+	{
+		public string ContentName { get; set; }
+		public bool ContentFlag { get; set; }
+		public Content(string content)
+		{
+			this.ContentName = content;
+		}
+		public Content(string content, bool contentFlag)
+		{
+			this.ContentName = content;
+			this.ContentFlag = contentFlag;
+		}
+	}
+}
