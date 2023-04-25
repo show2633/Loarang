1c9160d7fda504e085d748dbf3a98284de82c140
shinhyunwoo <show2633@naver.com>
2023-04-25 23:28:44 +0900

    [design]: 메뉴 버튼에 따른 caption 및 icon 변경

--- Loarang/ViewModels/NavigationVM.cs
+++ Loarang/ViewModels/NavigationVM.cs
@@ -1,17 +1,57 @@
-﻿using Loarang.Command;
+﻿using FontAwesome.Sharp;
+using Loarang.Command;
 using System.Windows.Input;
 
 namespace Loarang.ViewModels
 {
 	class NavigationVM : NavigationBase
 	{
-		public ICommand ContentsCommand { get; set; }		
+		private string _caption;
+		private IconChar _icon;
+
+		public ICommand ShowHomeCommand { get; set; }
+		public ICommand ShowContentsCommand { get; set; }
+		public ICommand ShowCharacterCommand { get; set; }
 		private void Content(object obj) => CurrentView = new ContentsVM();		
 		public NavigationVM()
 		{
-			ContentsCommand = new RelayCommand(Content);
+			ShowHomeCommand = new RelayCommand(ShowHomeViewCommand);
+			ShowContentsCommand = new RelayCommand(ShowContentsViewCommand);
+			ShowCharacterCommand = new RelayCommand(ShowCharacterViewCommand);
 
+			CurrentView = new HomeVM();
+		}
+
+		public string Caption
+		{
+			get { return _caption; }
+			set { _caption = value; OnPropertyChanged(); }
+		}
+
+		public IconChar Icon
+		{
+			get { return _icon; }
+			set { _icon = value; OnPropertyChanged(); }
+		}
+
+		private void ShowHomeViewCommand(object obj)
+		{
+			Caption = "홈";
+			Icon = IconChar.Home;
+			CurrentView = new HomeVM();
+		}
+
+		private void ShowContentsViewCommand(object obj)
+		{
+			Caption = "컨텐츠";
+			Icon = IconChar.CalendarCheck;
 			CurrentView = new ContentsVM();
+		}
+		private void ShowCharacterViewCommand(object obj)
+		{
+			Caption = "캐릭터";
+			Icon = IconChar.User;
+			CurrentView = new CharacterVM();
 		}
 	}
 }
