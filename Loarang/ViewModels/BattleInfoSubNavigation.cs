d52b0fbcec8279f4d1df4f6966477b866dd314b4
shinhyunwoo <show2633@naver.com>
2023-04-29 04:38:51 +0900

    [design]: 스킬 창 작업

--- Loarang/ViewModels/BattleInfoSubNavigation.cs
+++ Loarang/ViewModels/BattleInfoSubNavigation.cs
@@ -6,10 +6,13 @@ namespace Loarang.ViewModels
 	class BattleInfoSubNavigation : NavigationBase
 	{
 		public ICommand ShowStatsCommand { get; set; }
+		public ICommand ShowSkillCommand { get; set; }
 		private void ShowStats(object obj) => CurrentView = new StatsVM();
+		private void ShowSkill(object obj) => CurrentView = new SkillVM();
 		public BattleInfoSubNavigation()
 		{
 			ShowStatsCommand = new RelayCommand(ShowStats);
+			ShowSkillCommand = new RelayCommand(ShowSkill);
 
 			CurrentView = new StatsVM();
 		}
