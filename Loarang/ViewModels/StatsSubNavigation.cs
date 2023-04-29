373ca77f9dcfcc2a6fe556416ccb37c9eb929292
shinhyunwoo <show2633@naver.com>
2023-04-30 00:28:40 +0900

    [design]: 장비 창 작업, 능력치 창에 장비 창 보이도록 설정

---
+++ Loarang/ViewModels/StatsSubNavigation.cs
@@ -0,0 +1,23 @@
+﻿using Loarang.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Loarang.ViewModels
+{
+	class StatsSubNavigation : NavigationBase
+	{
+		public ICommand ShowEquipmentCommand { get; set; }
+		private void ShowEquipment(object obj) => CurrentView = new EquipmentVM();
+
+		public StatsSubNavigation()
+		{
+			ShowEquipmentCommand = new RelayCommand(ShowEquipment);
+
+			CurrentView = new EquipmentVM();
+		}
+	}
+}
